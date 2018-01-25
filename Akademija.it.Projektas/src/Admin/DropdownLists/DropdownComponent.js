7b3b46937bb718824a1793764d9a7403c7c50052
tkopalinskas <tkopalinskas@gmial.com>
2018-01-25 13:04:31 +0200

    working get on list

--- Akademija.it.Projektas/src/Admin/DropdownLists/DropdownComponent.js
+++ Akademija.it.Projektas/src/Admin/DropdownLists/DropdownComponent.js
@@ -3,6 +3,9 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 import ListofAdmins from './ListofAdmins';
+import ListofPatients from './ListofPatients';
+import ListofDoctors from './ListofDoctors';
+import ListofPharmasists from './ListofPharmacists'
 
 export default class DrawerUndockedExample extends React.Component {
 
@@ -13,7 +16,7 @@ export default class DrawerUndockedExample extends React.Component {
             showAdminList: false,
             showPatientList: false,
             showDoctorList: false,
-            showPharmasistList: false,
+            showPharmacistList: false,
         };
     }
 
@@ -24,12 +27,6 @@ export default class DrawerUndockedExample extends React.Component {
         this.setState({ [toggleName]: !this.state[toggleName] });
     }
     
-
-
-
-
-
-    // OpenListAdmins = () => this.setState({ openAdmin: !this.state.openAdmin});
 
     render() {
         return (
@@ -47,12 +44,21 @@ export default class DrawerUndockedExample extends React.Component {
                 >
                     <MenuItem onClick={this.handleToggle.bind(this, "showDrawer")}>Close</MenuItem>
                     <MenuItem onClick={this.handleToggle.bind(this, "showAdminList")}>List of Admins</MenuItem>
-                    <MenuItem onClick={this.handleOpenListPatients}>List of Patients</MenuItem>
-                    <MenuItem onClick={this.handleOpenListDoctors}>List of Doctors</MenuItem>
-                    <MenuItem onClick={this.handleOpenListPharmasists}>List of Pharmasists</MenuItem>
+                    <MenuItem onClick={this.handleToggle.bind(this, "showPatientList")}>List of Patients</MenuItem>
+                    <MenuItem onClick={this.handleToggle.bind(this, "showDoctorList")}>List of Doctors</MenuItem>
+                    <MenuItem onClick={this.handleToggle.bind(this, "showPharmacistList")}>List of Pharmasists</MenuItem>
                     <ListofAdmins 
                     open={this.state.showAdminList} 
                     closeAction={this.handleToggle.bind(this, 'showAdminList')} />
+                    <ListofPatients
+                    open={this.state.showPatientList} 
+                    closeAction={this.handleToggle.bind(this, 'showPatientList')} />
+                    <ListofDoctors
+                    open={this.state.showDoctorList} 
+                    closeAction={this.handleToggle.bind(this, 'showDoctorList')} />
+                    <ListofPharmasists
+                    open={this.state.showPharmacistList} 
+                    closeAction={this.handleToggle.bind(this, 'showPharmacistList')} />
                 </Drawer>
 
             </div>
