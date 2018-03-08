190c367b1082715e464f70cdf66ba3fefa05558e
KSalatka <35289059+KSalatka@users.noreply.github.com>
2018-03-08 13:20:26 +0200

    Update PharmacistRegistrationTest.java

--- sveikata/src/test/java/autotest/qa/ksalatka/PharmacistRegistrationTest.java
+++ sveikata/src/test/java/autotest/qa/ksalatka/PharmacistRegistrationTest.java
@@ -7,63 +7,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import page.MainPage;
-//@RunWith(Parameterized.class)
+@RunWith(Parameterized.class)
 public class PharmacistRegistrationTest {
 	WebDriver driver = new FirefoxDriver();
 	MainPage mainPage = new MainPage(driver);
 	private String usernames;
-//
-//	public PharmacistRegistrationTest(String usernames) {
-//		this.usernames = usernames;
-//	}
 
-//	@Parameters
-//	public static List<String> data() throws IOException {
-//
-//		return getTestData("src/test/resources/pharmacist/PharmacistUsernames.csv");
-//
-//	}
+	public PharmacistRegistrationTest(String usernames) {
+		this.usernames = usernames;
+	}
+
+	@Parameters
+	public static List<String> data() throws IOException {
+
+		return getTestData("src/test/resources/pharmacist/PharmacistUsernames.csv");
+
+	}
 
 	@Before
 	public void testSetup() {
-		driver.get("http://localhost:3000/#/admin");
-//		mainPage.inputUsername("AdminUserName");
-//		mainPage.inputPassword("AdminPassword");
-//		mainPage.clickLogin();
+		driver.get("http://localhost:8081/#/");
+		mainPage.inputUsername("admin1");
+		mainPage.inputPassword("admin1");
+		mainPage.clickLogin();
 		mainPage.clickRegisterNewUser();
 
 	}
 
 	@After
 	public void CloseBrowser() {
-	//	driver.close();
+		driver.close();
 	}
 	@Test
 	public void PharmacistRegistration() {
 		mainPage.clickRegisterPharmacist();
-		mainPage.inputFirstName("name");
-		mainPage.inputLastName("lastname");		
+		mainPage.inputFirstName("vardas");
+		mainPage.inputLastName("pavarde");		
 		mainPage.inputWorkPlace("workingPlace");
-		mainPage.inputPassword("asdasd");
-		mainPage.inputConfirmPassword("asdasd");
-		mainPage.inputUsername("asdasd");
+		mainPage.inputPassword("password");
+		mainPage.inputConfirmPassword("password");
+		mainPage.inputUsername(usernames);
 		Select selectWorkPlaceType = new Select(driver.findElement(By.className("typeOfWorkplace")));
 		selectWorkPlaceType.selectByIndex((int)Math.ceil(Math.random()*4));
 		mainPage.clickRegister();
-
-		
-		//Assert
+		Alert alert = driver.switchTo().alert(); 
+		String text = alert.getText(); 		
+		Assert.assertTrue("Registration failed",text.contains("Registracija sėkminga!"));
+		alert.dismiss();	
 	}
 
 	public static List<String> getTestData(String fileName) throws IOException {
