fcf6011ab0b6c5178dd39dd5bad9783c2fb2d1ac
KSalatka <35289059+KSalatka@users.noreply.github.com>
2018-03-13 11:47:56 +0200

    Add files via upload

---
+++ autotest/src/test/java/autotest/qa/ksalatka/PharmacistRegistrationTest.java
@@ -0,0 +1,84 @@
+package autotest.qa.ksalatka;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import page.MainPage;
+@RunWith(Parameterized.class)
+public class PharmacistRegistrationTest {
+	WebDriver driver = new FirefoxDriver();
+	MainPage mainPage = new MainPage(driver);
+	private String usernames;
+
+	public PharmacistRegistrationTest(String usernames) {
+		this.usernames = usernames;
+	}
+
+	@Parameters
+	public static List<String> data() throws IOException {
+
+		return getTestData("src/test/resources/pharmacist/PharmacistUsernames.csv");
+
+	}
+
+	@Before
+	public void testSetup() {
+		driver.get("http://localhost:8081/#/admin");
+/*		mainPage.inputUsername("admin1");
+		mainPage.inputPassword("admin1");
+		mainPage.clickLogin();*/
+		mainPage.clickRegisterNewUser();
+
+	}
+
+	@After
+	public void CloseBrowser() {
+		driver.close();
+	}
+	@Test
+	public void PharmacistRegistration() {
+		mainPage.clickRegisterPharmacist();
+		mainPage.inputFirstName("PharmacistName");
+		mainPage.inputLastName("PharmacistLastname");		
+		mainPage.inputWorkPlace("WorkingPlace");
+		mainPage.inputPassword("pharmacist");
+		mainPage.inputConfirmPassword("pharmacist");
+		mainPage.inputUsername(usernames);
+		Select selectWorkPlaceType = new Select(driver.findElement(By.className("typeOfWorkplace")));
+		selectWorkPlaceType.selectByIndex((int)Math.ceil(Math.random()*4));
+		mainPage.clickRegister();
+		Alert alert = driver.switchTo().alert(); 
+		String text = alert.getText(); 		
+		Assert.assertTrue("Registration failed",text.contains("Registracija sėkminga!"));
+		alert.dismiss();	
+	}
+
+	public static List<String> getTestData(String fileName) throws IOException {
+		List<String> records = new ArrayList<String>();
+		String record;
+
+		BufferedReader file = new BufferedReader(new FileReader(fileName));
+		while ((record = file.readLine()) != null) {
+			records.add(record);
+		}
+		file.close();
+		return records;
+
+	}
+}
---
+++ autotest/src/test/java/page/MainPage.java
@@ -0,0 +1,114 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MainPage extends AbstractPage{
+
+	//inputs
+	@FindBy(id = "inputFirstName")
+	private WebElement inputFirstname;
+	@FindBy(id = "inputLastName")
+	private WebElement inputLastname;
+	@FindBy(id = "inputUserName")
+	private WebElement inputUsername;
+	@FindBy(id = "inputPassword")
+	private WebElement inputPassword;
+	@FindBy(id = "inputRepeatedPassword")
+	private WebElement inputConfirmPassword;
+	@FindBy(id = "inputPersonalCode")
+	private WebElement inputPersonalCode;
+	@FindBy(id = "inputWorkplace")
+	private WebElement inputWorkPlace;
+
+	
+	
+	//buttons
+	@FindBy (id= "submitForm")
+	private WebElement buttonRegister;
+	@FindBy (css = "button[type='submit']")
+	private WebElement buttonLogin;
+	@FindBy (className = "newUserRegistration")
+	private WebElement buttonUserRegistration;
+	@FindBy(className = "adminRegistration")
+	private WebElement buttonAdminNewRegistration;
+	@FindBy(className = "registerPatient")
+	private WebElement buttonRegisterNewPatient;
+	@FindBy (className = "registerDoctor")
+	private WebElement  buttonRegisterNewDoctor;
+	@FindBy(className = "registerPharmacist")
+	private WebElement buttonRegisterNewPharmacist;
+	@FindBy(className = "registerAdmin")
+	private WebElement buttonRegisterNewAdmin;
+	@FindBy(className = "newUserRegistration")
+	private WebElement buttonCreateNewUser;
+	@FindBy(className = "userPopoverMenu")
+	private WebElement buttonMenu;
+	@FindBy(className = "logOut")
+	private WebElement buttonLogOut;
+	@FindBy(id = "generateDateOfBirthButton")
+	private WebElement buttonGenerateDate;
+	
+	
+	
+
+	public MainPage(WebDriver driver) {
+		super(driver);
+	}
+		public void inputFirstName(String name) {
+			inputFirstname.sendKeys(name);
+		}
+		public void inputLastName(String lastname) {
+			inputLastname.sendKeys(lastname);
+		}
+		public void inputUsername(String username) {
+			inputUsername.sendKeys(username);
+		}
+		public void inputPassword(String password) {
+			inputPassword.sendKeys(password);
+		}
+		public void inputConfirmPassword(String confirm) {
+			inputConfirmPassword.sendKeys(confirm);
+		}
+		public void inputPersonalCode(String code) {
+			inputPersonalCode.sendKeys(code);
+		}
+		public void inputWorkPlace(String workingPlace) {
+			inputWorkPlace.sendKeys(workingPlace);
+		}
+		public void clickRegister(){
+			buttonRegister.click();
+		}
+		public void clickLogin() {
+			buttonLogin.click();
+		}
+		public void clickRegisterAdmin() {
+			buttonRegisterNewAdmin.click();
+		}
+		public void clickRegisterPharmacist() {
+			buttonRegisterNewPharmacist.click();
+		}
+		public void clickRegisterDoctor() {
+			buttonRegisterNewDoctor.click();
+		}
+		public void clickRegisterPatient() {
+			buttonRegisterNewPatient.click();
+		}
+		public void clickRegisterNewUser() {
+			buttonCreateNewUser.click();
+		}
+		public void clickMenu() {
+			buttonMenu.click();
+		}
+		public void clickLogOut() {
+			buttonLogOut.click();
+		}
+		public void clickGenerateDate() {
+			buttonGenerateDate.click();
+		}
+	
+	
+	
+
+}
